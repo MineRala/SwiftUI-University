d89af4562536311d568233d99d217e8a590e44fd
mine rala <mine-rala@hotmail.com>
2021-07-18 12:51:46 +0300

    main page added

---
+++ SwiftUI-University/CircleImage.swift
@@ -0,0 +1,21 @@
+//
+//  CircleImage.swift
+//  SwiftUI-University
+//
+//  Created by Mine Rala on 16.07.2021.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("kampus")
+        
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
